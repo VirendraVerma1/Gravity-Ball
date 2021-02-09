692e8c7e27d69755a2fe2ac3b404c736f0a5986d
VirendraVerma-Viru <64251693+VirendraVerma-Viru@users.noreply.github.com>
2021-02-09 23:19:52 +0530

    Initial commit

---
+++ Assets/Script/BoxRotator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxRotator : MonoBehaviour
+{
+   //right for clock wise
+   //left for anticlockwise
+    public float rotateSpeed = 0.2f;
+    public void OnLeftRotation()
+    {
+        
+        StartCoroutine(RotateToDirection(-90));
+        
+
+    }
+
+    public void OnRightRotation()
+    {
+        
+        StartCoroutine(RotateToDirection(90));
+        
+    }
+
+    void FrezeBall()
+    {
+        GameObject.FindGameObjectWithTag("MyBall").GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    void UnFrezeBall()
+    {
+        GameObject.FindGameObjectWithTag("MyBall").GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    IEnumerator RotateToDirection(int rotate)
+    {
+        FrezeBall();
+        int n=rotate;
+        while (n != 0)
+        {
+            yield return new WaitForSeconds(rotateSpeed);
+            if (n > 0)
+            {
+                gameObject.transform.Rotate(Vector3.forward, 1f);
+                n--;
+            }
+            else
+            {
+                gameObject.transform.Rotate(Vector3.forward, -1f);
+                n++;
+            }
+        }
+        UnFrezeBall();
+    }
+}
---
+++ Assets/Script/FinishTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "MyBall")
+        {
+            Destroy(col.gameObject);
+            print("Game Won");
+        }
+    }
+
+    
+}
